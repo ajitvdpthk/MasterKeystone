8e3e531b074008594fbf57d82fa0a478c090143d
Ajit Vedpathak <gnx2714@varian.com>
2018-02-12 12:19:08 +0530

    Commit

---
+++ src/main/java/com/hybridFramwork/com/TestBase/TestBase.java
@@ -0,0 +1,13 @@
+package com.hybridFramwork.com.TestBase;
+import org.openqa.selenium.WebDriver;
+public class TestBase {
+	String browser;
+	public void getBrowser() {
+		if(System.getProperty("os.name").contains("windows")){
+			if (browser.equalsIgnoreCase("")) {
+				
+			}
+		}
+	}
+	
+}
